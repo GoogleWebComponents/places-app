a5d0f1db82cc0f48576fc3f277eba66897763b64
Eric Bidelman <ebidel@gmail.com>
2015-09-25 16:57:35 -0700

    More cleanup

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 #
 # Copyright 2015 Eric Bidelman <ericbidelman@chromium.org>
 
-vulcanize elements.html \
-    --inline-script --inline-css --strip-comments | \
-    crisper -h elements.vulcanize.html -j elements.vulcanize.js
+# vulcanize elements.html \
+#     --inline-script --inline-css --strip-comments | \
+#     crisper -h elements.vulcanize.html -j elements.vulcanize.js
+
+polybuild --maximum-crush elements.html
